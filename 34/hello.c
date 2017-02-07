e259f7cc130a1ead9d5bf0bca55d75cca28e3560
WangYihang <wangyihanger@gmail.com>
2017-02-08 06:02:10 +0800

    用整型数字作为函数指针使用(危险 , 容易溢出)

--- 34/hello.c
+++ 34/hello.c
@@ -20,6 +20,16 @@ int main(){
 	printf("Result : %d\n", computer(x, y, p_handle));
 	p_handle = (divid);
 	printf("Result : %d\n", computer(x, y, p_handle));
+
+	/* 将一个整型变量转换为函数指针 */
+	int temp = &add;
+	int result = ((int (*)(int x, int y))(temp))(10, 10);
+	printf("%d\n", result);
+	// 这里说明一个整型变量通过强制类型转换是可以作为函数指针来使用的
+	printf("sizeof(int) = %d\n", sizeof(int));
+	printf("sizeof(int*) = %d\n", sizeof(int*));
+	printf("整型在低地址区域可以当作函数指针来用 , 但是整型变量只有 4 字节 , 所以一旦指针的值大于 4 字节能表示的最大范围就会溢出 , 因此不建议这样用 , 反过来想 , 函数指针为代码段 , 地址都很低 , 大概用的时候也不太可能出错 , 但是如果用作指向栈内存的变量的时候就很容易出错\n");
+	
 	return 0;
 }
 
