25cdfc40926274b410559345b1ca762b7c2b687a
WangYihang <wangyihanger@gmail.com>
2017-02-07 21:51:16 +0800

    理解C指针

---
+++ 23/wrong/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+int main(int argc, char *argv[])
+{
+	int a = 1;
+	int b = 2;
+	// swap(a, b);
+	printf("Number a : %d\nNumber b : %d\n", a, b);
+	return 0;
+}
