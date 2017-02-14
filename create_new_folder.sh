71951d6ff7126eca8de25d0da957650b35c1ac69
WangYihang <wangyihanger@gmail.com>
2017-02-14 20:06:14 +0800

    修改 create_new_folder.sh , 创建新文件夹的时候创建模版文件

--- create_new_folder.sh
+++ create_new_folder.sh
@@ -15,8 +15,36 @@ get_max_number
 max_number=$?
 echo "新文件夹创建成功 , 当前最大序号为 : " $max_number
 cd $max_number
+
 touch ./README.md
 echo '目标' >> ./README.md
 echo '```' >> ./README.md
 echo '' >> ./README.md
 echo '```' >> ./README.md
+
+touch ./foo.c
+echo '#include "foo.h"' >> ./foo.c
+echo '' >> ./foo.c
+echo 'int main(int argc, char *argv[]){' >> ./foo.c
+echo '' >> ./foo.c
+echo '	return 0;' >> ./foo.c
+echo '}' >> ./foo.c
+
+touch ./foo.h
+echo '#ifndef __FOO_H__' >> ./foo.h
+echo '#define __FOO_H__' >> ./foo.h
+echo '' >> ./foo.h
+echo '#include <stdio.h>' >> ./foo.h
+echo '' >> ./foo.h
+echo 'int main(int argc, char *argv[]);' >> ./foo.h
+echo '' >> ./foo.h
+echo '#endif' >> ./foo.h
+
+touch ./Makefile
+echo 'foo:foo.o' >> ./Makefile
+echo '	gcc -o foo foo.o' >> ./Makefile
+echo 'foo.o:foo.c' >> ./Makefile
+echo '	gcc -c foo.c' >> ./Makefile
+echo 'clean:' >> ./Makefile
+echo '	rm ./foo.o' >> ./Makefile
+echo '	rm ./foo' >> ./Makefile
