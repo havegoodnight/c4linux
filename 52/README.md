386c19b402f4ebb03ef8bb1098f98b707a5b8602
WangYihang <wangyihanger@gmail.com>
2017-02-22 08:37:20 +0800

    52

---
+++ 52/README.md
@@ -0,0 +1,12 @@
+目标
+```
+统计字符串出现的次数 , 并给出上下文(当前的做法是打印前后若干个字符 , 那么如何打印所在行?)
+```
+使用
+```
+./search [filepath] [str]
+./search ./data.txt never
+./search ./data.txt lose
+./search /etc/passwd `whoami`
+```
+
---
+++ 52/search.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void print_neiberhood(char *content, int size);
+int count_string(char *father, char *son);
+
+int main(int argc, char *argv[]) {
+
+	if (argc != 3){
+		printf("Usage : \n\t%s [filename] [string]\n", argv[0]);
+		exit(1);
+	}
+
+	FILE *fd;
+	char buffer[1024] = {0};
+	char *filename = argv[1];
+	char *son = argv[2];
+	int counter = 0;
+
+	fd = fopen(filename, "r");
+
+	if (fd == NULL) {
+		write(2, "Failed to open file.\n", 20);
+		exit(1);
+	}
+
+	while (!feof(fd)) {
+		memset(buffer, 0, sizeof(buffer));
+		fgets(buffer, sizeof(buffer) - 1, fd);
+		int temp = count_string(buffer, son);
+		if (temp > 0) {
+			counter += temp;
+		}
+	}
+
+	fclose(fd);
+
+	printf("[ %d ] times of [ %s ] found in the file : [ %s ].\n", counter, son, filename);
+
+	return 0;
+}
+
+void print_neiberhood(char *content, int size){
+	int i;
+	for (i = 0; i < size; i++){
+		printf("%c", content[i - 9]);
+	}
+	printf("\n");
+}
+
+int count_string(char *father, char *son){
+	int counter = 0;
+	int pointer = 0; // 指针
+	int length = strlen(son);
+	// printf("length of son is : %d\n", length);
+	// printf("Comparing : [%s] and [%s]\n", father, son);
+	while(pointer < 1024){
+		int i = 0;
+		int flag = 1;
+		for (i = 0; i < length; i++){
+			// printf("Comparing : [ %c ] and [ %c ]\n", *(father+pointer+i), *(son+i));
+			if((*(father + pointer + i)) != (*(son + i))){
+				flag = 0;
+				break;
+			}
+		}
+		if (flag) { // found
+			print_neiberhood(&father[pointer], 18);
+			counter++;
+			pointer++;
+		} else { // not found
+			pointer += (i+1);
+			// printf("Now pointer is [%d]\n", pointer);
+		}
+	}
+	return counter; 
+}
