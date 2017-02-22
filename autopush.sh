eb283b9b88f17201b9b777f6ee63d0c3e8c29033
WangYihang <wangyihanger@gmail.com>
2017-02-22 09:03:25 +0800

    Update autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ $# == 1 ]; then
-	git add . && git commit -m "$1" && git push
+	cd "$1"
+	echo "Workspace : `pwd`"
+	comment=`head README.md -n 3 | tail -n 1`
+	echo "Comment : ${comment}"
+	git add . && git commit -m "${comment}" && git push
 else
-	echo -e "Usage : \r\n\t$0 \"Init commit\""
+	echo -e "Usage : \r\n\t$0 [ folder_name ]"
 fi
